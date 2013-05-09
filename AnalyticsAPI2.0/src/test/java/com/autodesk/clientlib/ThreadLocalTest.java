62add3a16e5623f1b7d0813ced24d04a9ef05866
leandro.mora <leandro.jorge.mora@gmail.com>
2013-05-09 12:36:28 -0300

    ThreadLocalTest example finished and comitted

--- AnalyticsAPI2.0/src/test/java/com/autodesk/clientlib/ThreadLocalTest.java
+++ AnalyticsAPI2.0/src/test/java/com/autodesk/clientlib/ThreadLocalTest.java
@@ -3,46 +3,65 @@ package com.autodesk.clientlib;
 import junit.framework.Assert;
 
 import org.apache.log4j.Logger;
-import org.junit.Before;
 import org.junit.Test;
 
 import com.autodesk.clientlib.KeyPair.Key;
 
 public class ThreadLocalTest implements Runnable{
-	static final Logger example_logger=Logger.getLogger(ThreadLocalTest.class);
+//Fields -----------------------------
+	
+	private static String threadValue="";	
+
+//Getters and Setters -----------------------------
 		
-//----------------------------------------
+	public String getThreadValue() {
+		return threadValue;
+	}
+
+	public void setThreadValue(String threadValue) {
+		ThreadLocalTest.threadValue = threadValue;
+	}
+	
+//Auxiliar Methods ----------------------------------------
+
+	public void methodB(){
+		LogItem.getInstance().put(Key.DURATION, "1000");
+
+	}
+
+//Thread Method ----------------------------------------
+	
+	@Override
+	public synchronized void run() {
+		LogItem.getInstance().put(FacetsKeys.CURRENT_STATE, "aborted");
+		this.setThreadValue(LogItem.getInstance().outputEvent());
+
+	}
+	
+// Test ----------------------------------------
+	
+	/**
+	 * Test that validates the functionality of Thread Local Storage
+	 * 
+	 * @author t_moral
+	 * @throws InterruptedException 
+	 */
+	@Test
+	public void testMaintThread() throws InterruptedException{
+		ThreadLocalTest myExample=new ThreadLocalTest();
 		
-		public void methodB(){
-			LogItem.getInstance().put(Key.DURATION, "1000");
-
-			LogItem.getInstance().get(Key.API_CATEGORY);
-		}
-
-//----------------------------------------
-
-		@Override
-		public void run() {
-			LogItem.getInstance().put(FacetsKeys.CURRENT_STATE, "aborted");
-			Assert.assertEquals("{\"api_category\":\"file\",\"duration\":\"1000\"}", LogItem.getInstance().outputEvent());
-			Assert.assertEquals("{\"compute_job_current_state\":\"aborted\"},\"duration\":\"1000\"}", LogItem.getInstance().outputEvent());			
-
-		}
+		LogItem.getInstance().put(Key.API_CATEGORY,"file");
+		Assert.assertEquals("{\"api_category\":\"file\"}", LogItem.getInstance().outputEvent());
 		
-//----------------------------------------
-		@Test
-		public void testMaintThread() throws InterruptedException {
-			ThreadLocalExample myExample=new ThreadLocalExample();
-			
-			LogItem.getInstance().put(Key.API_CATEGORY,"file");
-			Assert.assertEquals("{\"api_category\":\"file\"}", LogItem.getInstance().outputEvent());
-			
-			myExample.methodB();
-			Assert.assertEquals("{\"api_category\":\"file\",\"duration\":\"1000\"}", LogItem.getInstance().outputEvent());
-			//Returns
-			Thread t=new Thread(myExample);
-			t.start();
-			t.join();
-		}
+		myExample.methodB();
+		Assert.assertEquals("{\"api_category\":\"file\",\"duration\":\"1000\"}", LogItem.getInstance().outputEvent());
+
+		Thread t=new Thread(myExample);
+		t.start();
+		t.join();
+		Assert.assertEquals("{\"compute_job_current_state\":\"aborted\"}",threadValue);
+		Assert.assertEquals("{\"api_category\":\"file\",\"duration\":\"1000\"}", LogItem.getInstance().outputEvent());
+
+	}
 
 }
